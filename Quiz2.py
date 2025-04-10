909a2d64d3cf25317d7735aa23a85335cb91b460
zrwinte <winterszachary2003@gmail.com>
2025-04-10 14:40:55 -0500

    Update Quiz2.py

--- Quiz2.py
+++ Quiz2.py
@@ -16,3 +16,30 @@ def check_buttons():
     red = red_button.is_pressed
     green = green_button.is_pressed
     blue = blue_button.is_pressed
+    if red and not green and not blue:
+        print("The Red Button has been pressed!")
+        rgb_led.color = (1, 0, 0)  
+    elif green and not red and not blue:
+        print("The Green Button has been pressed!")
+        rgb_led.color = (0, 1, 0)  
+    elif blue and not red and not green:
+        print("The Blue Button has been pressed!")
+        rgb_led.color = (0, 0, 1)  
+    elif red and green and not blue:
+        print("Red and Green Make Yellow!")
+        rgb_led.color = (1, 1, 0)  
+    elif red and blue and not green:
+        print("Red and Blue Make Magenta!")
+        rgb_led.color = (1, 0, 1)  # 
+    elif green and blue and not red:
+        print("Green and Blue Make Cyan")
+        rgb_led.color = (0, 1, 1)  
+    elif red and green and blue:
+        print("All Colors Combined Make White!")
+        rgb_led.color = (1, 1, 1)  
+    else:
+        # No buttons pressed
+        rgb_led.color = (0, 0, 0)
+
+
+        
